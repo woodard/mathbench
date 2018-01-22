1f942ef07261ca59d1f0d080537fc52a513d197f
Ben Woodard <woodard@redhat.com>
2018-01-22 19:51:49 +0100

    Massive rework of code.

--- time_math2.C
+++ time_math2.C
@@ -5,96 +5,60 @@
 #include <math.h>
 #include <sys/time.h>
 
+#include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <utility>
+#include <vector>
 
-static struct timeval start_ts, end_ts;
-
-void start_timer()
-{
-  if (gettimeofday (&start_ts, NULL) != 0)
-    {
-      fprintf (stderr, "Unable to get time of day, exiting\n");
-      exit (1);
-    }
+inline void Gettimeofday(struct timeval &ts){
+  if (gettimeofday (&ts, NULL) != 0) {
+    fprintf (stderr, "Unable to get time of day, exiting\n");
+    exit (1);
+  }
 }
 
-void end_timer(double count)
+double time_exp (int count, double val_in)
 {
-  double seconds, ns_per;
-#ifndef BGQ
-  double ghz=2.9;
-#else
-  double ghz=1.6;
-#endif
-    
-
-  if (gettimeofday (&end_ts, NULL) != 0)
-    {
-      fprintf (stderr, "Unable to get time of day, exiting\n");
-      exit (1);
-    }
+  volatile double val = val_in;
+  double sum = 0;;
+  struct timeval start_ts, end_ts;
+  
+  Gettimeofday(start_ts);
+  for (int i=1; i <= count; i++) {
+    sum += exp(val);
+  }
+  Gettimeofday(end_ts);
+  
   /* Calculate run time */
-  seconds = ((double)end_ts.tv_sec + ((double)end_ts.tv_usec * 1e-6)) -
-    ((double)start_ts.tv_sec + ((double)start_ts.tv_usec * 1e-6));
+  double seconds, ns_per;
+  suseconds_t usecs = end_ts.tv_usec-start_ts.tv_usec;
+  time_t secs = end_ts.tv_sec-start_ts.tv_sec;
+  seconds = secs+usecs*1e-6;
   ns_per = (seconds * 1.0E9)/count;
-  printf ("Took %lf seconds and %.0lfns or ~%.0lf cycles at %.1fGHz\n",
-	  seconds, ns_per, ns_per * ghz, ghz);
-  /* Get start counter again so can do another end_timer (); */
-  if (gettimeofday (&start_ts, NULL) != 0)
-    {
-      fprintf (stderr, "Unable to get time of day, exiting\n");
-      exit (1);
-    }
+
+  return ns_per;
 }
 
-void time_exp (int count, double val_in)
-{
-  int i;
-  volatile double val = val_in;
-  double sum = 0;;
-  printf ("---\n");
-  printf ("Timing %i calls to exp(%.18f)\n", count, val);
-  start_timer();
-  for (i=1; i <= count; i++)
-    {
-      sum += exp(val);
-    }
-  end_timer((double)count);
-  printf ("Sum of %i calls to exp(%.13a) is %.13a\n", count, val, sum);
-
-}
-
-void time_pow (int count, double val_in)
-{
-  int i;
-  volatile double val = val_in;
-  double sum = 0;;
-  printf ("---\n");
-  printf ("Timing %i calls to pow(%.18f, %.18f)\n", count, val, 1.5);
-  start_timer();
-  for (i=1; i <= count; i++)
-    {
-      sum += pow(val, 1.5);
-    }
-  end_timer((double)count);
-  printf ("Sum of %i calls to pow(%.13a, %.13a) is %.13a\n", count, val,
-	  1.5, sum);
-
-}
 int main(int argc, char **argv)
 {
   std::ifstream infile(argv[1]);
   std::string line;
+  std::vector< std::pair<double, double> > results;
   while (std::getline(infile, line)){
     std::istringstream iss(line);
     double a;
     if (iss.peek()=='#')
       continue;
     if (!(iss >> a)) { break; } // error
-    time_exp(100000, a);
-    
+    results.push_back(std::pair<double,double>(a, time_exp(100000, a)));
   }
+
+  for( std::vector< std::pair<double,double> >::iterator i=results.begin();
+       i!=results.end();
+       i++)
+    std::cout << i->first << ' ' << i->second << std::endl;    
+
 }
 
